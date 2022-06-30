933e8510e81b40c3932420cd21066678d3cfb263
rmoroz20 <87720362+rmoroz20@users.noreply.github.com>
2022-06-30 18:42:54 -0400

    i hate timeout errors

--- server/scraper.py
+++ server/scraper.py
@@ -1,8 +1,25 @@
 from rotten_tomatoes_scraper.rt_scraper import CelebrityScraper
+from rotten_tomatoes_scraper.rt_scraper import MovieScraper
 
-celebrity_scraper = CelebrityScraper(celebrity_name='jack nicholson')
-celebrity_scraper.extract_metadata(section='highest')
+celebrity_scraper = CelebrityScraper(celebrity_name='austin butler')
+celebrity_scraper.extract_metadata(section='filmography')
 movie_titles = celebrity_scraper.metadata['movie_titles']
 
+bad_movies = []
 print(movie_titles)
-#['On a Clear Day You Can See Forever', 'The Shooting', 'Chinatown', 'Broadcast News']
+
+for title in movie_titles:
+    
+    movie_scraper = MovieScraper(movie_title = title)
+    movie_scraper.extract_metadata()
+    print(movie_scraper.metadata)
+    
+    # if (int((movie_scraper.metadata['Score_Rotten'])) < 60) and (int((movie_scraper.metadata['Score_Audience'])) > 70):
+    #     bad_movies.append(title)
+
+    # if int(movie_stats['Score_Rotten']) <= 60 and int(movie_stats['Score_Audience']) >= 70:
+    #     bad_movies.append(title)
+
+print(bad_movies)
+
+print(movie_scraper.metadata)
