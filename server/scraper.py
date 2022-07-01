83e39719530fa0d6e105b8a7ed5bc5aeae805fad
rmoroz20 <87720362+rmoroz20@users.noreply.github.com>
2022-06-30 21:17:13 -0400

    web scraping timout fix

--- server/scraper.py
+++ server/scraper.py
@@ -1,7 +1,7 @@
 from rotten_tomatoes_scraper.rt_scraper import CelebrityScraper
 from rotten_tomatoes_scraper.rt_scraper import MovieScraper
 
-celebrity_scraper = CelebrityScraper(celebrity_name='austin butler')
+celebrity_scraper = CelebrityScraper(celebrity_name='Demi Lovato')
 celebrity_scraper.extract_metadata(section='filmography')
 movie_titles = celebrity_scraper.metadata['movie_titles']
 
@@ -9,17 +9,14 @@ bad_movies = []
 print(movie_titles)
 
 for title in movie_titles:
-    
     movie_scraper = MovieScraper(movie_title = title)
     movie_scraper.extract_metadata()
-    print(movie_scraper.metadata)
-    
-    # if (int((movie_scraper.metadata['Score_Rotten'])) < 60) and (int((movie_scraper.metadata['Score_Audience'])) > 70):
-    #     bad_movies.append(title)
 
-    # if int(movie_stats['Score_Rotten']) <= 60 and int(movie_stats['Score_Audience']) >= 70:
-    #     bad_movies.append(title)
+    try:
+        if int((movie_scraper.metadata['Score_Rotten'])) < 60 and (int((movie_scraper.metadata['Score_Audience'])) > 60):
+            bad_movies.append(title)
+    except ValueError:
+        print("This doesn't have a rating!")
+
 
 print(bad_movies)
-
-print(movie_scraper.metadata)
