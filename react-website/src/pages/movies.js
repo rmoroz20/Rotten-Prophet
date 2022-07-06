cd304a15ee2d0530cb966a2358192187bb8105f3
rmoroz20 <87720362+rmoroz20@users.noreply.github.com>
2022-07-05 22:08:02 -0400

    button styling

--- react-website/src/pages/movies.js
+++ react-website/src/pages/movies.js
@@ -10,15 +10,10 @@ const Movies = () => {
     <div>
       <Navbar />
       <h1>Find your movie here!</h1>
-      
-
 
       <div className="container">
-        <h1>React Button</h1>
         <div className="inner-container">
-          <Button color="blue" name="Blue" />
-          <Button color="green" name="Green" />
-          <Button color="red" name="Red" />
+          <Button color="orange" name="Blue" />
         </div>
       </div>
 
