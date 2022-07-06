e31608795982a19f0cb874befdee622ac76354f0
rmoroz20 <87720362+rmoroz20@users.noreply.github.com>
2022-07-05 22:38:41 -0400

    home image styling

--- react-website/src/pages/index.js
+++ react-website/src/pages/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Card from '../components/Card/Card.js';
+import './index.css'
 import Navbar from '../components/Navigation/Navbar.js';
+import prophet from '../images/prophet.png'
+
   
 const Home = () => {
   return (
+
     <div>
       <Navbar />
       <Card />
+      <img src={prophet} class='center'></img>
     </div>
   );
 };
