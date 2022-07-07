aeb10d74be26bef7e98d39c9f5c1908e3309afcb
rmoroz20 <87720362+rmoroz20@users.noreply.github.com>
2022-07-07 12:19:43 -0400

    reading csv data

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,6 +1,8 @@
 from rotten_tomatoes_scraper.rt_scraper import CelebrityScraper
 from rotten_tomatoes_scraper.rt_scraper import MovieScraper
 
+import csv
+import random
 
 #getting bad movies by known celebrity name
 celebrity_scraper = CelebrityScraper(celebrity_name='Demi Lovato')
@@ -8,7 +10,6 @@ celebrity_scraper.extract_metadata(section='filmography')
 movie_titles = celebrity_scraper.metadata['movie_titles']
 
 bad_movies = []
-print(movie_titles)
 
 for title in movie_titles:
     movie_scraper = MovieScraper(movie_title = title)
@@ -18,8 +19,16 @@ for title in movie_titles:
         if int((movie_scraper.metadata['Score_Rotten'])) < 60 and (int((movie_scraper.metadata['Score_Audience'])) > 60):
             bad_movies.append(title)
     except ValueError:
-        print("This doesn't have a rating!")
-
-
+        pass
 
 print(bad_movies)
+
+# MOVIE FROM RANDOM CELEBRITY
+with open("actors.csv", "r") as actor_csv_file:
+    csv_reader = csv.reader(actor_csv_file)
+    # This skips over the first line in the .csv (first_name, last_name)
+    next(csv_reader)
+    print(random.choice([line[0] for line in csv_reader]))
+
+
+
