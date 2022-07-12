a2dca4e19ee1741d1a6647d83588bb6b77dcdb5c
rmoroz20 <87720362+rmoroz20@users.noreply.github.com>
2022-07-12 17:42:06 -0400

    spacing on contact page

--- react-website/src/pages/contact.js
+++ react-website/src/pages/contact.js
@@ -5,7 +5,9 @@ const Contact = () => {
 return (
 	<div>
 	<Navbar />
-	<h1>Hi! I'm Rebecca Moroz, a Software Engineering major at Drexel University and the developer of this site!</h1>
+	<h1>Hi!</h1> 
+	
+	<h2>I'm Rebecca Moroz, a Software Engineering major at Drexel University and the developer of this site!</h2>
 	</div>
 );
 };
