b2787f7cbbf95e517474545405dd3081867cb99e
rmoroz20 <87720362+rmoroz20@users.noreply.github.com>
2022-07-13 13:27:42 -0400

    contact page css

--- react-website/src/pages/contact.js
+++ react-website/src/pages/contact.js
@@ -1,3 +1,4 @@
+import { TableRow } from '@mui/material';
 import React from 'react';
 import Navbar from "../components/Navigation/Navbar";
 import me from '../images/me.jpg'
@@ -6,18 +7,32 @@ import './contact.css'
 const Contact = () => {
 return (
 	<div>
-	<Navbar />
-	<h1>Hi!</h1> 
+		<Navbar />
+		<h1>Hi!</h1> 
 	
-	<div class="flex-container">
-		<div class="flex-child">
-			<h2>I'm Rebecca Moroz, a Software Engineering major at Drexel University and the developer of this site!</h2>
+		<div class="flex-container">
+			<div class="intro-div">
+				<h2>I'm Rebecca Moroz, a Software Engineering major at Drexel University and the developer of this site!</h2>
+				<div class="arrow">
+					<h2>This is me enjoying some mango water ice! -----{">"}</h2>
+					<h3>(which I love almost as much as bad movies)</h3>
+				</div>
+
+				<div class="contact-info">
+					<h2>Feel free to email me at <a href='mailto: rlm338@drexel.edu'>rlm338@drexel</a> with any questions or issues.</h2>
+					<h3>Suggestions are always welcome!</h3>
+				</div>
+			
+			</div>
+
+			<div class="img-div">
+				<img src={me} alt=''></img>
+			</div>
+
+		
+			
 		</div>
-	
-		<div class="flex-child">
-			<img src={me} class='center' alt=''></img>
-		</div>
-	</div>
+
 	</div>
 );
 };
