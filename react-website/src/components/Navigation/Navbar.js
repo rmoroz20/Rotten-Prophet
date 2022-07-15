5c2661ade2f77d8530f46ec317066cb1da9ab5ad
rmoroz20 <87720362+rmoroz20@users.noreply.github.com>
2022-07-15 14:48:31 -0400

    navbar now has logo

--- react-website/src/components/Navigation/Navbar.js
+++ react-website/src/components/Navigation/Navbar.js
@@ -1,11 +1,16 @@
 import React from "react";
 import './Navbar.css';
+import logo from './images/rp-logo-small.png'
 
 
 const Navbar = () => {
   return (
     <div>
+    
     <nav class="navMenu">
+    <a href="/" class="logo">
+      <img src={logo} alt=''></img>
+    </a>
       <a href="/">Home</a>
       <a href="movies">Movies</a>
       <a href="about">About</a>
